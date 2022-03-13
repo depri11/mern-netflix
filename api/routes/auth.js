d6c129e4fb97efa4ccf7c06ac6747595b1c39c5a
devriwirlandika <wirlandikadevri@gmail.com>
2022-03-13 17:33:51 +0700

    add cryptojs ( auth register )

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 // REGISTER
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
   });
   try {
     const user = await newUser.save();
